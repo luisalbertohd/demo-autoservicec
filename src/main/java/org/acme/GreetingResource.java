67125fb1dd675e5d63ca6b4e6c5d5e6fe1ff5cda
Luis Hernandez <luis.alberto.hd@gmail.com>
2024-12-10 00:59:21 +0000

    hjhj

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello REST BELEN 2";
+        return "Hello REST BELEN 2B";
     }
 }
